323b4fe679602c5aad5290985bb53c3d4d5d2485
Alberto Reyer <newLoki@users.noreply.github.com>
2021-12-17 17:52:03 +0100

    TE-9873: New glue infrastructure (#8823)
    TE-9873 Glue Backend API code infrastructure

--- src/Spryker/Glue/Kernel/Backend/Locator.php
+++ src/Spryker/Glue/Kernel/Backend/Locator.php
@@ -5,7 +5,7 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace Spryker\Zed\Kernel;
+namespace Spryker\Glue\Kernel\Backend;
 
 use Spryker\Client\Kernel\ClientLocator;
 use Spryker\Service\Kernel\ServiceLocator;
@@ -17,13 +17,11 @@ use Spryker\Zed\Kernel\Persistence\QueryContainerLocator;
 class Locator extends AbstractLocatorLocator
 {
     /**
-     * @var \Spryker\Zed\Kernel\Locator
+     * @var static
      */
     private static $instance;
 
     /**
-     * @internal
-     *
      * @return self
      */
     public static function getInstance(): self
@@ -44,9 +42,10 @@ class Locator extends AbstractLocatorLocator
         if ($this->locator === null) {
             $this->locator = [
                 new FacadeLocator(),
-                new QueryContainerLocator(),
                 new ClientLocator(),
+                new ResourceLocator(),
                 new ServiceLocator(),
+                new QueryContainerLocator(),
             ];
         }
         $bundleProxy->setLocators($this->locator);
