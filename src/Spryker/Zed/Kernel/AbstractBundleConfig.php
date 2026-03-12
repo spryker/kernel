29eed081c79fd96e13e25bc971f739fdc8ea18c5
Dmitriy Krainiy <dmitriy.krainiy@spryker.com>
2026-03-12 13:11:14 +0100

    CC-37021: Backoffice configuration (#375)
    CC-37021 Product Availability: Back Office Configuration UI

--- src/Spryker/Zed/Kernel/AbstractBundleConfig.php
+++ src/Spryker/Zed/Kernel/AbstractBundleConfig.php
@@ -7,10 +7,43 @@
 
 namespace Spryker\Zed\Kernel;
 
+use Generated\Shared\Transfer\ConfigurationValueRequestTransfer;
 use Spryker\Shared\Kernel\AbstractBundleConfig as SharedAbstractBundleConfig;
 use Spryker\Shared\Kernel\SharedConfigResolverAwareTrait;
 
 abstract class AbstractBundleConfig extends SharedAbstractBundleConfig
 {
     use SharedConfigResolverAwareTrait;
+
+    /**
+     * @param string $key
+     * @param mixed|null $default
+     * @param array<\Generated\Shared\Transfer\ConfigurationScopeTransfer> $configurationScopes
+     *
+     * @return mixed
+     */
+    protected function getModuleConfig(string $key, $default = null, array $configurationScopes = []): mixed
+    {
+        if (!interface_exists('\Spryker\Zed\Configuration\Business\ConfigurationFacadeInterface')) {
+            return $default;
+        }
+
+        /** @var \Generated\Zed\Ide\AutoCompletion&\Spryker\Shared\Kernel\LocatorLocatorInterface $locator */
+        $locator = Locator::getInstance();
+
+        $requestTransfer = (new ConfigurationValueRequestTransfer())
+            ->setKey($key);
+
+        foreach ($configurationScopes as $configurationScopeTransfer) {
+            $requestTransfer->addScope($configurationScopeTransfer);
+        }
+
+        $configValue = $locator->configuration()->facade()->getConfigurationValue($requestTransfer);
+
+        if ($configValue !== null) {
+            return $configValue;
+        }
+
+        return $default;
+    }
 }
