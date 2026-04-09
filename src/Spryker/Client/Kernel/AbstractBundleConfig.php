23baae323f83682e08740be87359971f3d890dd7
Veronika Golska <86779768+geronica@users.noreply.github.com>
2026-04-09 11:44:28 +0200

    CC-36908 added BO configuration for Algolia credentials and Catalog/CMS search providers (#444)
    CC-36908 Algolia: Backoffice configuration page

--- src/Spryker/Client/Kernel/AbstractBundleConfig.php
+++ src/Spryker/Client/Kernel/AbstractBundleConfig.php
@@ -7,10 +7,43 @@
 
 namespace Spryker\Client\Kernel;
 
+use Generated\Shared\Transfer\ConfigurationValueRequestTransfer;
 use Spryker\Shared\Kernel\AbstractBundleConfig as SharedAbstractBundleConfig;
 use Spryker\Shared\Kernel\SharedConfigResolverAwareTrait;
 
 abstract class AbstractBundleConfig extends SharedAbstractBundleConfig
 {
     use SharedConfigResolverAwareTrait;
+
+    /**
+     * @param string $key
+     * @param mixed|null $default
+     * @param array<\Generated\Shared\Transfer\ConfigurationScopeTransfer> $configurationScopes
+     *
+     * @return mixed
+     */
+    protected function getModuleConfig(string $key, $default = null, array $configurationScopes = []): mixed
+    {
+        if (!interface_exists('\Spryker\Client\Configuration\ConfigurationClientInterface')) {
+            return $default;
+        }
+
+        /** @var \Generated\Client\Ide\AutoCompletion&\Spryker\Shared\Kernel\LocatorLocatorInterface $locator */
+        $locator = Locator::getInstance();
+
+        $requestTransfer = (new ConfigurationValueRequestTransfer())
+            ->setKey($key);
+
+        foreach ($configurationScopes as $configurationScopeTransfer) {
+            $requestTransfer->addScope($configurationScopeTransfer);
+        }
+
+        $configValue = $locator->configuration()->client()->getConfigurationValue($requestTransfer);
+
+        if ($configValue !== null) {
+            return $configValue;
+        }
+
+        return $default;
+    }
 }
